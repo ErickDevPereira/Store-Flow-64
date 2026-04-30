3771e8dc94f2e7f444274ad75ffaac717ca07630
erick <erick1204200100@gmail.com>
2026-04-30 16:16:39 -0300

    hamburguer menu is working as expected for each page inside the website

--- src/frontend/inner_pages/hamburguer-code/interact.js
+++ src/frontend/inner_pages/hamburguer-code/interact.js
@@ -4,9 +4,7 @@ if (window.innerWidth < 750) {
 }
 
 function viewOpt() {
-    const opt = document.getElementById("hamburguer-opt");
     const opt_status = opt.style.display;
-    console.log(opt_status);
     if (opt_status === "none") {
         opt.style.display = "flex";
         opt.style.flexDirection =  "column";
@@ -16,7 +14,6 @@ function viewOpt() {
 }
 
 function correctDisplayBug() {
-    const opt = document.getElementById("hamburguer-opt");
     if (window.innerWidth < 750) {
         opt.style.display = "none";
     } else {
